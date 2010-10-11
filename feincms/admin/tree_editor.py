7e80be962508c47365f4ce60c37abad65b5d80ad
James O'Donnell <james@bibleglo.local>
2010-10-11 10:49:19 -0400

    fixed what should have never been committed

--- feincms/admin/tree_editor.py
+++ feincms/admin/tree_editor.py
@@ -139,44 +139,44 @@ class ChangeList(main.ChangeList):
                     self.query_set.values_list('lft', 'rght', 'tree_id')]
             if clauses:
                 self.query_set = self.model._default_manager.filter(reduce(lambda p, q: p|q, clauses))
-            if self.opts.app_label == self.opts.module_name == 'page':
-                self.list_per_page = 5000
-                self.show_all = True
-                self.page_num = 0
-                paginator = Paginator(self.query_set, self.list_per_page)
-                # Get the number of objects, with admin filters applied.
-                result_count = paginator.count
-  
-                # Get the total number of objects, with no admin filters applied.
-                # Perform a slight optimization: Check to see whether any filters were
-                # given. If not, use paginator.hits to calculate the number of objects,
-                # because we've already done paginator.hits and the value is cached.
-                if not self.query_set.query.where:
-                    full_result_count = result_count
-                else:
-                    full_result_count = self.root_query_set.count()
-  
-                can_show_all = result_count <= main.MAX_SHOW_ALL_ALLOWED
-                multi_page = result_count > self.list_per_page
-  
-                # Get the list of objects to display on this page.
-                if (self.show_all and can_show_all) or not multi_page:
-                    result_list = self.query_set._clone()
-                else:
-                    try:
-                        result_list = paginator.page(self.page_num+1).object_list
-                    except InvalidPage:
-                        result_list = ()
-  
-                self.result_count = result_count
-                self.full_result_count = full_result_count
-                self.result_list = result_list
-                self.can_show_all = can_show_all
-                self.multi_page = multi_page
-                self.paginator = paginator
-  
+        if self.opts.app_label == self.opts.module_name == 'page':
+            self.list_per_page = 5000
+            self.show_all = True
+            self.page_num = 0
+            paginator = Paginator(self.query_set, self.list_per_page)
+            # Get the number of objects, with admin filters applied.
+            result_count = paginator.count
+
+            # Get the total number of objects, with no admin filters applied.
+            # Perform a slight optimization: Check to see whether any filters were
+            # given. If not, use paginator.hits to calculate the number of objects,
+            # because we've already done paginator.hits and the value is cached.
+            if not self.query_set.query.where:
+                full_result_count = result_count
             else:
-                return super(ChangeList, self).get_results(request)
+                full_result_count = self.root_query_set.count()
+
+            can_show_all = result_count <= main.MAX_SHOW_ALL_ALLOWED
+            multi_page = result_count > self.list_per_page
+
+            # Get the list of objects to display on this page.
+            if (self.show_all and can_show_all) or not multi_page:
+                result_list = self.query_set._clone()
+            else:
+                try:
+                    result_list = paginator.page(self.page_num+1).object_list
+                except InvalidPage:
+                    result_list = ()
+
+            self.result_count = result_count
+            self.full_result_count = full_result_count
+            self.result_list = result_list
+            self.can_show_all = can_show_all
+            self.multi_page = multi_page
+            self.paginator = paginator
+
+        else:
+            return super(ChangeList, self).get_results(request)
                 
 main.ChangeList = ChangeList
 
